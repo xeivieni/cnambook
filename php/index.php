16294db58140061603f00bef5e874088d3d8f122
clement pro <clement.mondion@gmail.com>
2015-11-19 13:19:39 +0100

    index.php

--- php/index.php
+++ php/index.php
@@ -47,16 +47,21 @@ $status = array_merge($status1, $status2);
     $stmt->execute(array('id' => $statut["idstatut"]));
     $likes = $stmt->fetchAll();
     $count = $stmt->rowCount();
+    $stmt2 = $conn->prepare('SELECT * FROM Users WHERE iduser=:id');
+    $stmt2->execute(array('id' => $statut["iduser"]));
+    $owner = $stmt2->fetchAll();
+
     ?>
+
     <div class="panel panel-default">
-        <div class="panel-heading"><a href="#" class="pull-right">Modifier</a><img src="//placehold.it/50x50" class="img-circle"><h4><?php echo $nom; echo " "; echo $prenom;?></h4></div>
+        <div class="panel-heading"><img src="//placehold.it/50x50" class="img-circle"><h4><a><?php echo $owner[0]["nom"]; echo " "; echo $owner[0]["prenom"];?></a></h4></div>
         <div class="panel-body">
             <?php echo $statut["texte"]; ?>
             <div class="clearfix"></div>
             <hr>
 
             <p> <?php echo $count; ?> likes<a href="#" data-toggle="modal"
-                                              data-target="#basicModal">Voir les amis qui ont liké</a></p>
+                                              data-target="#likes<?php echo $statut["idstatut"]?>">Voir les amis qui ont liké</a></p>
 
             <hr>
             <form>
@@ -67,10 +72,34 @@ $status = array_merge($status1, $status2);
                     <input type="text" class="form-control" placeholder="Add a comment..">
                 </div>
             </form>
-
         </div>
     </div>
 
+    <div class="modal fade" id="basicModal1" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                    <h4 class="modal-title" id="myModalLabel">Ils ont liké</h4>
+                </div>
+                <div class="modal-body">
+                    <?php foreach($likes as $like){
+                        $stmt = $conn->prepare('SELECT nom, prenom FROM Users WHERE iduser=:id');
+                        $stmt->execute(array('id' => $like["iduser"]));
+                        $name = $stmt->fetchAll();
+                        echo $name[0]["nom"];
+                        echo " ";
+                        echo $name[0]["prenom"];
+                        echo "<br>";
+                    }
+                    ?>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="modal fade" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
