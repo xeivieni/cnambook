577b3d2d8ed5b4a9bcf5ff0614e12bee6737eaa2
jonathanquie <jonathanquie@hotmail.fr>
2015-11-19 15:45:48 +0100

    - Treatment of the "idsection" during registration

--- php/register.php
+++ php/register.php
@@ -14,13 +14,28 @@
     $password = $_GET["inputPassword"];
     $inscriptioDate = "19/11/15";
 
+
+    $stmt2 = $conn->prepare('SELECT idsection FROM Section WHERE nom=:sectionName AND annee =:yearSection');
+    $stmt2->execute(array('sectionName' => $section, 'yearSection' => $sectionYear));
+
+    $result = $stmt2->fetchAll();
+    $count = $stmt2->rowCount();
+
+    if ($count  == 1) {
+        $idsection = $result[0]["idsection"];
+    }
+    else
+    {
+        $idsection = 0;
+    }
+
     $stmt = $conn->prepare('INSERT INTO Users(nom, prenom, date_naissance, lien_photo, date_inscription, mail,
-    ville_origine, ville_residence, password) VALUES (:lastname, :firstname, :birthdaydate, :photolink, :inscriptiondate,
-    :email, :hometown, :cityresidency, MD5(:pass))');
+    ville_origine, ville_residence, idsection, password) VALUES (:lastname, :firstname, :birthdaydate, :photolink, :inscriptiondate,
+    :email, :hometown, :cityresidency, :idsection ,MD5(:pass))');
 
     $stmt->execute(array('lastname' => $lastName, 'firstname' => $firstName, 'birthdaydate' => $birthdayDate, 'photolink' =>
     $photo, 'inscriptiondate' => $inscriptioDate, 'email' => $email, 'hometown' => $hometown, 'cityresidency' => $cityResidency
-    , 'pass' => $password));
+    , 'idsection' => $idsection ,'pass' => $password));
 
     header("Location: index.php");
 
